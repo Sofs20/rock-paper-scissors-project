328b4429249c1f1121b290022fc45c58b2f4ae3b
Sofs20 <barbarasofia20@gmail.com>
2023-01-26 23:37:02 +0000

    game running with buttons

--- rps.js
+++ rps.js
@@ -1,10 +1,43 @@
-/*
-Your game is going to play against the computer,
-so begin with a function called getComputerChoice 
-that will randomly return either ‘Rock’, ‘Paper’ or 
-‘Scissors’. We’ll use this function in the game 
-to make the computer’s play. 
+/* In our UI, the player should be able to play the game
+by clicking on buttons rather than typing their answer
+in a prompt.
+
+For now, remove the logic that plays exactly five rounds.
+
+Create three buttons, one for each selection. Add an event
+listener to the buttons that call your playRound function with
+the correct playerSelection every time a button is clicked. 
+(you can keep the console.logs for this step)
+Add a div for displaying results and change all of your console.
+logs into DOM methods.
+
+Display the running score, and announce a winner of the game 
+once one player reaches 5 points.
+You will likely have to refactor (rework/rewrite) your original
+code to make it work for this. That’s OK! Reworking old code is
+an important part of a programmer’s life.
 */
+
+const btns = document.querySelectorAll(".choiceBtn");
+btns.forEach((choiceBtn) => {
+
+    choiceBtn.addEventListener("click",() => {
+
+        let playerSelection = choiceBtn.textContent;
+        let computerSelection = getComputerChoice();
+        let game = playRound(playerSelection, computerSelection);
+        const gameResults = document.querySelector(".results");
+        gameResults.textContent = game;
+        
+        if(playerWin === 5){
+            gameResults.textContent=  "You won!!";
+        }
+        else if (computerWin === 5){
+            gameResults.textContent=  "Computer won!!";
+        }
+    });
+
+});
 
 function getComputerChoice() {
 
@@ -29,71 +62,25 @@ case-insensitive (so users can input rock, ROCK, RocK
 or any other variation).
 */
 
-let playerWin = 1;
-let playerLose = 0;
-let playerTie = 0;
-let computerWin = 1;
-let computerLose = 0;
-let computerTie = 0;
+let playerWin = 0;
+let computerWin = 0;
 
 function playRound(playerSelection, computerSelection) {
 
-
+    
     if ((playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "scissors" && computerSelection === "rock") ||
         (playerSelection === "paper" && computerSelection === "scissors")) {
-        return "You lose!";
+            computerWin = computerWin + 1;
+            return "You lose!";
     } else if (playerSelection === "rock" && computerSelection === "scissors" ||
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")) {
-        return "You win!";
+            playerWin = playerWin + 1;
+            return "You win!";
     } else if (playerSelection === "rock" && computerSelection === "rock" ||
         (playerSelection === "scissors" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "paper")) {
-        return "It's a tie!";
+            return "It's a tie!";
     }
-
 }
-
-/*
-Write a NEW function called game(). Call the playRound
-function inside of this one to play a 5 round game 
-that keeps score and reports a winner or loser at 
-the end.
-Remember loops? This is a great opportunity to use 
-one to play those five rounds.
-Use prompt() to get input from the user. 
-*/
-
-function game() {
-
-    for (let i = 0; i < 5; i++) {
-
-        let playerSelection = prompt("Rock, Paper or Scissors?");
-        playerSelection.toLowerCase();
-        let computerSelection = getComputerChoice();
-        let playRoundResult = playRound(playerSelection, computerSelection);
-        console.log(playRoundResult);
-
-        if (playRoundResult === "You win!") {
-            playerWin++;
-        } else if (playRoundResult === "You lose!") {
-            computerWin++;
-        } else if (playRoundResult === "It's a tie!") {
-            playerTie++;
-            computerTie++;
-        }
-    }
-
-    if (playerWin > computerWin) {
-        return "You are the winner!";
-    } else if (playerWin < computerWin) {
-        return "Computer is the winner!";
-    } else if (playerWin === computerWin) {
-        return "It's a tie!!";
-    }
-
-}
-
-let finalScore = game();
-alert(finalScore);
